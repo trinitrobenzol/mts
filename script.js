8e42d1ce0351c752b16d47bbf308c5a2b80cb62b
trinitrobenzol <73372422+trinitrobenzol@users.noreply.github.com>
2022-03-03 13:12:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+  text.innerText = 'Вдох';
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Задержи дыхание';
+
+    setTimeout(() => {
+      text.innerText = 'Выдох';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(breathAnimation, totalTime);
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Source: https://kenwheeler.github.io/slick/
+
+*/
+
+
+/*
+The following executes Ken's Slick Slider with several options.
+*/
+
+$('.quotes').slick({
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 6000,
+  speed: 800,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+
+
+/*
+To prevent the flashing of unstyled content (FOUC) I created a class ".no-fouc"
+which hides the slider.  When everything is ready to roll, I simply remove the
+.no-fouc class from the slider section using the script below.  I placed the CSS snippet in the head of the HTML
+page so that it's loaded before other things for obvious reasons.  What about folks with JS turned off?  
+Well, I don't worry about them too much anymore.  Oh my.  I feel the heat from the flames already.  :) 
+*/
+
+$( document ).ready(function() {
+$('.no-fouc').removeClass('no-fouc');
+});
+
+
+
+$(document).ready(function() {
+  var btn = $(".button");
+  btn.click(function() {
+    btn.toggleClass("paused");
+    return false;
+  });
+});
+
+    let a=0;document.getElementsByTagName("i")[0].addEventListener("click",function(){
+if(a==0){
+document.getElementsByTagName("audio")[0].play();a++;document.querySelector("i").style.opacity="1";
+}else{
+document.querySelector("audio").pause();a--;document.querySelector("i").style.opacity=".5";
+}
+});
